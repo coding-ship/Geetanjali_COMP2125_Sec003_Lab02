1fdc21df022397a8571f82dd65ff80c5e6709b57
Geetanjali <311297sg@gmail.com>
2023-06-07 09:51:14 -0700

    Initial Commit

---
+++ Geetanjali_COMP2125_Sec003_Lab02/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Geetanjali_COMP2125_Sec003_Lab02
+//
+//  Created by user242668 on 6/7/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("j")
+print("hello world")
